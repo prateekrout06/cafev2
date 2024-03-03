5f26b899431aaf97bb849e9ced6fc3586263c312
Ethan <20065@wc.school.nz>
2024-03-03 14:14:31 +1300

    Video 5 Completed

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from aifc import Error
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 import sqlite3
 from sqlite3 import Error
+from flask_bcrypt import Bcrypt
 
-DATABASE = "C:/Users/GGPC/PycharmProjects/Smile/smile.db"
+# DATABASE = "C:/Users/GGPC/PycharmProjects/Smile/smile.db" # Desktop
+DATABASE = "C:/Users/ethan/PycharmProjects/SmileCafe/smile.db"  # Laptop
 
 app = Flask(__name__)
 
@@ -28,7 +28,7 @@ def render_menu_page(cat_id):
     con = create_connection(DATABASE)
     query = "SELECT name, description, volume, image, price FROM products WHERE cat_id=?"
     cur = con.cursor()
-    cur.execute(query, (cat_id, ))
+    cur.execute(query, (cat_id,))
     product_list = cur.fetchall()
     query = "SELECT id, name FROM category"
     cur = con.cursor()
@@ -43,4 +43,44 @@ def render_contact_page():
     return render_template('contact.html')
 
 
-app.run(host='0.0.0.0', debug=True)
+@app.route('/login', methods=['POST', 'GET'])
+def render_login():
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def render_signup():
+    if request.method == 'POST':
+        print(request.form)
+        fname = request.form.get('fname').title().strip()
+        lname = request.form.get('lname').title().strip()
+        email = request.form.get('email').lower().strip()
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if password != password2:
+            return redirect('\signup?error=Passwords+do+not+match')
+
+        if len(password) < 8:
+            return redirect('\signup?error=Passwords+must+be+at+least+8+characters')
+
+        con = create_connection(DATABASE)
+        query = 'INSERT INTO user (fname, lname, email, password) VALUES (?, ?, ?, ?)'
+        cur = con.cursor()
+
+        try:
+            cur.execute(query, (fname, lname, email, password))
+        except sqlite3.IntegrityError:
+            con.close()
+            return redirect('\signup?error=Email+is+already+used')
+
+        con.commit()
+        con.close()
+
+        return redirect('\login')
+
+    return render_template('signup.html')
+
+
+if __name__ == '__main__':
+    app.run()
